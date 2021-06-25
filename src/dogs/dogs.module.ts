61d2dbc80c8d3f4c82dc8b333f9896386ee97837
Jveen-D <d85066965@icloud.com>
2021-06-25 15:24:28 +0800

    全局模块

--- src/dogs/dogs.module.ts
+++ src/dogs/dogs.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DogsController } from './dogs.controller';
 import { DogsService } from './dogs.service';
-import { CatsModule} from 'src/cats/cats.module'
 @Module({
-  imports:[CatsModule],
   controllers: [DogsController],
   providers: [DogsService]
 })
